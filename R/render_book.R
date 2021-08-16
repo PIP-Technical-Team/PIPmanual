9f871e572c91faf7744fdb4138329e5b65a5afda
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-08-16 16:46:45 -0400

    add bs4, but leave gitbook

--- R/render_book.R
+++ R/render_book.R
@@ -9,13 +9,15 @@ file <- "price_framework.Rmd"
 bookdown::render_book(file, "bookdown::gitbook",
                       preview = TRUE)
 
+# bookdown::render_book(file, "bookdown::bs4_book",
+#                       preview = TRUE)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #---------   render the whole book   ---------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
-
+# bookdown::render_book("index.Rmd", "bookdown::bs4_book")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #----- serve book to see modification on the fly   --------
